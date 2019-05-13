f18802848b80c76dc71068f00e2f34597f4370f0
Karolis Stulgys <k.stulgys@gmail.com>
2019-05-14 09:30:23 +1000

    wip

--- src/Interests.js
+++ src/Interests.js
@@ -1,5 +1,5 @@
-import React from "react"
-import Section from "./Section"
+import React from "react";
+import Section from "./Section";
 
 export default function Interests() {
   return (
@@ -7,16 +7,18 @@ export default function Interests() {
       <div class="item mb-5">
         <p class="text-justify mb-2">
           My name is Karolis, I’m highly motivated and passionate self-taught
-          frontend engineer.
+          frontend engineer. I'm fascinated about nutrition, human performance,
+          strength training and everything in between.
         </p>
         <p class="text-justify mb-2">
-          I found myself constantly exploring the latest technology advancements
-          in the front-end web development world and having a great desire to
-          learn new things. Code and design are my passion. I have what it takes
-          to be a great developer and currently I'm actively looking for new
-          opportunities. My github profile might not shine with the complex
-          projects, but I encourage you to asses my skills with a homework
-          assignment or on site and I will not make you disappointed.
+          During the pas few years I found myself constantly exploring the
+          latest technology advancements in the front-end web development world
+          and having a great desire to learn new things. Code and design are my
+          passion. I have what it takes to be a great developer and currently
+          I'm actively looking for new opportunities. My github profile might
+          not shine with the complex projects, but I encourage you to asses my
+          skills with a homework assignment or on site and I will not make you
+          disappointed.
         </p>
         <p class="text-justify mb-3">
           Thank you for your time, and let’s get in touch to talk about the
@@ -26,5 +28,5 @@ export default function Interests() {
         <cite title="Source Title">- Karolis</cite>
       </div>
     </Section>
-  )
+  );
 }
