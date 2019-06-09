47f02dac6f730584ed48278d909589de1af0ebe5
Karolis Stulgys <k.stulgys@gmail.com>
2019-06-09 16:38:14 +1000

    wip

--- src/Interests.js
+++ src/Interests.js
@@ -1,32 +1,32 @@
-import React from "react";
-import Section from "./Section";
+import React from "react"
+import Section from "./Section"
 
 export default function Interests() {
   return (
     <Section id="Interests">
       <div class="item mb-5">
         <p class="text-justify mb-2">
-          I'm Karolis - highly motivated and passionate self-taught frontend
-          engineer. I'm fascinated about nutrition, human performance, strength
-          training and everything in between.
+          I'm Karolis - Passionate full-stack web developer successful at
+          completing lucrative and labor-intensive projects. Highly skilled and
+          educated.
         </p>
         <p class="text-justify mb-2">
           During the pas few years I found myself constantly exploring the
-          latest technology advancements in the front-end web development world
-          and having a great desire to learn new things. Code and design are my
-          passion. I have what it takes to be a great developer and currently
-          I'm actively looking for new opportunities to show my skills. My
-          github profile might not shine with the complex projects, but I
-          encourage you to asses my skills with a homework assignment or on site
-          and I will not make you disappointed.
+          latest technology advancements in the web development world and having
+          a great desire to learn new things. Code and design is my passion. I
+          also have a great interest in human performance, strength training and
+          longevity.
         </p>
         <p class="text-justify mb-3">
-          Thank you for your time, and let’s get in touch to talk about the
-          future.
+          I'm always looking for opportunities to work for well-established
+          organization appreciating dedication and hard work. If you have
+          something in mind I'd be glad to discuss my abilities and background
+          in more detail. Thank you for your time and let's get in touch to talk
+          about the future!
         </p>
 
         <cite title="Source Title">- Karolis</cite>
       </div>
     </Section>
-  );
+  )
 }
